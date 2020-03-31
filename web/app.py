3104aa064e2a97076efcc6d3151edec308c23254
Almonacid98 <gabriel98almonacid@hotmail.com>
2020-03-31 20:58:27 -0300

    Mas

--- web/app.py
+++ web/app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 import os
 from dotenv import load_dotenv
+from main import create_app
 
 load_dotenv()
+
 app = Flask(__name__)
+
+apps = create_app()
+apps.app_context().push()
 
 if __name__ == '__main__':
     app.run(debug = True, port = os.getenv("PORT"))
+
+
