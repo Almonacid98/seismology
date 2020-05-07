2073fca08cae387b72813ed84beaa1f5c891eea8
Almonacid98 <gabriel98almonacid@hotmail.com>
2020-05-07 17:20:53 -0300

    Se importaron recursos de user y users

--- api/main/resources/__init__.py
+++ api/main/resources/__init__.py
@@ -4,3 +4,5 @@ from .Seism import UnverifiedSeism as UnverifiedSeismResource
 from .Seism import UnverifiedSeisms as UnverifiedSeismsResource
 from .Sensors import Sensor as SensorResource
 from .Sensors import Sensors as SensorsResource
+from .Users import User as UserResource
+from .Users import Users as UsersResource
